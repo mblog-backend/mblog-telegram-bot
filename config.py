71de938d8c7cd4b0998cfb7d2a4ff170471a7f70
cooolr <32413353+cooolr@users.noreply.github.com>
2023-06-02 17:07:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ TELEGRAM_TOKEN = ""
 MBlog_Backend_URL = ""
 
 # mblog token
-MBlog_Token = ""
+MBlog_TOKEN = ""
 
 # 可见权限
 Visibility = "PRIVATE"
@@ -22,10 +22,10 @@ if env.get("TELEGRAM_TOKEN"):
 if env.get("PROXY_URL"):
     PROXY_URL = env.get("PROXY_URL")
 if env.get("MBlog_Backend_URL"):
-    PROXY_URL = env.get("MBlog_Backend_URL")
-if env.get("MBlog_Token"):
-    PROXY_URL = env.get("MBlog_Token")
+    MBlog_Backend_URL = env.get("MBlog_Backend_URL")
+if env.get("MBlog_TOKEN"):
+    MBlog_TOKEN = env.get("MBlog_TOKEN")
 if env.get("Visibility"):
-    PROXY_URL = env.get("Visibility")
+    Visibility = env.get("Visibility")
 
 proxies = {"http": PROXY_URL, "https": PROXY_URL}
